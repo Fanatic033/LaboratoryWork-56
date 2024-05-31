c885d1a69e8ddb34271d2a12eb0695ae34c5b359
ILzat <ilzat.sadykov.2006@bk.ru>
2024-05-31 16:18:01 +0600

    'Добавил типы для ингредиентов,цены и img.еще Cделал массив с ингредиентами'

---
+++ src/Components/Ingridients.ts
@@ -0,0 +1,13 @@
+import meatImg from '../assets/meat.png';
+import cheeseImg from '../assets/cheese.png';
+import saladImg from './assets/salad.png';
+import baconImg from './assets/bacon.png';
+
+import {Ingredient} from './types';
+
+export const INGREDIENTS: Ingredient = [
+  {name: 'Meat', price: 50, image: meatImg},
+  {name: 'Cheese', price: 50, image: cheeseImg},
+  {name: 'Salad', price: 50, image: saladImg},
+  {name: 'Bacon', price: 50, image: baconImg},
+];
